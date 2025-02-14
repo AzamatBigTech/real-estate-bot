625010641b04ed01b34192c264a58c5687a97567
AzamatBigTech <azamat.mukazhanov@gmail.com>
2025-02-14 23:31:37 +0500

    Initial commit

--- config.py
+++ config.py
@@ -2,9 +2,11 @@
 TELEGRAM_TOKEN = "7869709666:AAEh57tVtQFWijeVTDu0DWw_x-HpbnhiXEM"
 DEEPSEEK_API_KEY = "sk-03a9ac0e7b50478a898a4fef81155565"
 DB_CONFIG = {
-    "host": "https://ujobuszvekoqighffycs.supabase.co",
+    "host": "ujobuszvekoqighffycs.supabase.co",
+    "port": 5432,
     "user": "postgres",
     "password": "xvst1h2",
     "database": "postgres"
 }
-#SENTRY_DSN = "ВАШ_SENTRY_DSN"  # Опционально, для мониторинга ошибок
+#SENTRY_DSN = "ВАШ_SENTRY_DSN"  
+# Опционально, для мониторинга ошибок
