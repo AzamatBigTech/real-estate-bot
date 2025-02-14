e58adc76770edc7bc01984d6342f433a680fdd94
AzamatBigTech <azamat.mukazhanov@gmail.com>
2025-02-14 22:50:17 +0500

    Initial commit

--- db_handler.py
+++ db_handler.py
@@ -9,8 +9,9 @@ class Database:
         with self.conn.cursor() as cur:
             cur.execute(
                 "INSERT INTO analyses (user_id, location, area, price, type, result) VALUES (%s, %s, %s, %s, %s, %s)",
-                (user_id, data[0], float(data[1]), int(data[2]), data[3], result)
-            self.conn.commit()
+                (user_id, data[0], float(data[1]), int(data[2]), data[3], result)  # Закрываем скобку тут
+            )  # Добавили закрывающую скобку
+            self.conn.commit()  # Теперь это на правильном уровне
     
     def close(self):
         self.conn.close()
