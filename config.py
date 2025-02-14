21f51a0784a17b7377e8e58abf0edebb801212cc
AzamatBigTech <azamat.mukazhanov@gmail.com>
2025-02-14 23:57:01 +0500

    Initial commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ TELEGRAM_TOKEN = "7869709666:AAEh57tVtQFWijeVTDu0DWw_x-HpbnhiXEM"
 DEEPSEEK_API_KEY = "sk-03a9ac0e7b50478a898a4fef81155565"
 DB_CONFIG = {
     "host": "ujobuszvekoqighffycs.supabase.co",
-    "port": 5432,
+    "port": 8080,
     "user": "postgres",
     "password": "xvst1h2",
     "database": "postgres"
