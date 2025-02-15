1c06c0e12c450a97b9b0dfd57b23ff54857d8cd3
AzamatBigTech <azamat.mukazhanov@gmail.com>
2025-02-15 11:41:30 +0500

    Первый коммит

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 TELEGRAM_TOKEN = "7869709666:AAEh57tVtQFWijeVTDu0DWw_x-HpbnhiXEM"
 DEEPSEEK_API_KEY = "sk-03a9ac0e7b50478a898a4fef81155565"
 DB_CONFIG = {
-    "host": "pooler.ujobuszvekoqighffycs.supabase.co",
+    "host": "aws-0-eu-central-1.pooler.supabase.com",
     "port": 6543,
-    "user": "postgres",
+    "user": "postgres.ujobuszvekoqighffycs",
     "password": "xvst1h2",
     "dbname": "postgres",
     "sslmode": "require"  # Обязательно
